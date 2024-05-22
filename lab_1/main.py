f66f98bcd9102c2126758336ff49918bba17f0d5
thegr8nick <kudashevnikitushka@yandex.ru>
2024-05-22 20:18:30 +0400

    second task is completed

--- lab_1/main.py
+++ lab_1/main.py
@@ -38,6 +38,38 @@ def caesar_cipher(path_input: str, shift_number: int) -> str:
             encrypted += letter
     return encrypted
 
+
+def count_frequency(path_input: str) -> dict[str, int]:
+    """
+    Counting frequency of letters in input file
+    :param path_input: str path of file with encrypted text
+    :return: dict of frequency letters
+    """
+    input_string = reader(path_input)
+    c = Counter(input_string)
+    result = c.most_common()
+    return {letter: key / sum(tup[1] for tup in result) for letter, key in result}
+
+
+def decrypt_text(input_text: str, encrypted_frequency_list: list[str]) -> str:
+    """
+    Encrypting text using algorithm frequency analysis
+    :param input_text: str path of file with encrypted text
+    :param encrypted_frequency_list: list of symbols by their frequency in descending order
+    :return: str decrypted string
+    """
+    try:
+        arr_encrypt_text = []
+
+        dictionary = dict(zip(encrypted_frequency_list, FREQUENCY_OF_DECRYPTED_TEXT))
+        for letter in input_text:
+            arr_encrypt_text.append(dictionary[letter])
+        text_for_decrypt = ''.join(arr_encrypt_text)
+        return text_for_decrypt
+    except Exception as e:
+        logging.error(f"Error in decryption: {e}\n")
+
+
 if __name__ == '__main__':
     paths_dict = json_loader(PATHS)
 
@@ -51,3 +83,8 @@ if __name__ == '__main__':
     writer(T1_TEXT_CODE, caesar_cipher(T1_TEXT, SHIFT))
     json_dumper(dict(zip(reader(T1_TEXT), caesar_cipher(T1_TEXT, SHIFT))), T1_KEY)
     logging.info('Task 1 completed successful')
+
+    writer(T2_TEXT, decrypt_text(reader(T2_COD3), list(freq[0] for freq in count_frequency(T2_COD3))))
+    json_dumper(dict(zip(reader(T2_COD3),
+                         decrypt_text(reader(T2_COD3), list(freq[0] for freq in count_frequency(T2_COD3))))), T2_KEY)
+    logging.info('Task 2 completed successful')
