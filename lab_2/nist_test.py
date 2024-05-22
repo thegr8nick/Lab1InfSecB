f8a5fa00b33ee5accb9c2bdfe708770ad82358aa
thegr8nick <kudashevnikitushka@yandex.ru>
2024-05-22 20:41:49 +0400

    longest sequence in block test

--- lab_2/nist_test.py
+++ lab_2/nist_test.py
@@ -52,6 +52,36 @@ def consecutive_bits_test(sequence: str) -> float:
         logging.error(f"Error during the test execution: {ex}\n")
 
 
+def longest_sequence_in_block_test(seq: str) -> float:
+    """
+    Perform the longest run of ones in a block test and return the p-value.
+    :param seq: str input binary sequence
+    :return: float p-value of the test
+    """
+    try:
+        block_max_len = {}
+        for step in range(0, len(seq), MAX_LENGTH_BLOCK):
+            block = seq[step:step + MAX_LENGTH_BLOCK]
+            max_length, length = 0, 0
+            for bit in block:
+                length = length + 1 if bit == "1" else 0
+                max_length = max(max_length, length)
+            block_max_len[max_length] = block_max_len.get(max_length, 0) + 1
+
+        v = {1: 0, 2: 0, 3: 0, 4: 0}
+        for i in block_max_len:
+            key = min(i, 4)
+            v[key] += block_max_len[i]
+
+        xi_square = 0
+        for i in range(4):
+            xi_square += math.pow(v[i + 1] - 16 * PI[i], 2) / (16 * PI[i])
+
+        return mpmath.gammainc(3 / 2, xi_square / 2)
+    except Exception as ex:
+        logging.error(f"Error during the test execution: {ex}\n")
+
+
 if __name__ == "__main__":
     sequences = json_loader(SEQUENCE_PATH)
     tests_cpp = sequences["cpp"]
@@ -59,5 +89,7 @@ if __name__ == "__main__":
 
     logging.info(f'frequency_bitwise_test for cpp: {frequency_bitwise_test(tests_cpp)}')
     logging.info(f'consecutive_bits_test for cpp: {consecutive_bits_test(tests_cpp)}')
+    logging.info(f'longest_sequence_in_block_test for cpp: {longest_sequence_in_block_test(tests_cpp)}')
     logging.info(f'frequency_bitwise_test for java: {frequency_bitwise_test(tests_java)}')
     logging.info(f'consecutive_bits_test for java: {consecutive_bits_test(tests_java)}')
+    logging.info(f'longest_sequence_in_block_test for java: {longest_sequence_in_block_test(tests_java)}')
